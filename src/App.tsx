88d13745fb9029f7a416f79677213913fb0e7912
miriam <miriam_glzr@hotmail.com>
2022-06-07 22:51:12 +0200

    contain content img size height

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import "./styles.css";
-import HomePage from "./scenes/HomePage";
-import VideoGame from "./scenes/VideoGame";
+import { lazy } from "react";
+const HomePage = lazy(() => import("./scenes/HomePage"));
+const VideoGame = lazy(() => import("./scenes/VideoGame"));
 import { Routes, Route } from "react-router-dom";
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/game" element={<VideoGame />} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/" element={<HomePage />} />
+			<Route path="/game" element={<VideoGame />} />
+		</Routes>
+	);
 }
